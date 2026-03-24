12c5d6ee361e18550e803ad64fc6c7d6be7e8bf9
qgallouedec2 <kinikouns@gmail.com>
2026-03-23 18:06:30 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="trl",
-    version="0.1.0",
+    version="0.1.0", 
     packages=find_packages(),
     python_requires=">=3.9",
     install_requires=[
